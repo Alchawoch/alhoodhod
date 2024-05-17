3dca185996ab0385c577ecbbce1fae4ed05fc786
Alchawoch <mohamadalchawoch77@gmail.com>
2024-05-17 20:30:35 +0300

    update

--- js/script.js
+++ js/script.js
@@ -30,3 +30,21 @@ sun.onclick = function() {
         sun.src = "img/sun.png";
     }
 }
+
+window.onscroll = function(){
+    scrollFunction();
+};
+
+function scrollFunction() {
+  var mybutton = document.getElementById("ttpbtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function ScrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
